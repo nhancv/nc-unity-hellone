7733871b12586645ccb8ef21f2e9930fe1b9038d
Nhan Cao <nhancv92@gmail.com>
2018-03-19 00:47:13 +0700

    update

---
+++ Assets/Scripts/OpenUrlBss.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenUrlBss : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		TapSelect(); 
+	}	
+
+	void TapSelect() 
+	{
+		foreach (Touch touch in Input.touches) 
+		{
+			if (touch.phase == TouchPhase.Began) 
+			{
+				Ray ray = Camera.main.ScreenPointToRay(touch.position);
+				RaycastHit hit;
+				if (Physics.Raycast (ray, out hit)) 
+				{
+//					Debug.Log(hit.collider.gameObject.name);
+					if (hit.collider.gameObject.name == "VideoWall") {
+						Application.OpenURL ("https://beesightsoft.com");
+					}
+
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/OpenUrlNhancv.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenUrlNhancv : MonoBehaviour {
+
+	void Update () 
+	{
+		if (Input.GetKeyDown("escape"))
+			Application.Quit();
+
+		TapSelect(); 
+	}	
+
+	void TapSelect() 
+	{
+		foreach (Touch touch in Input.touches) 
+		{
+			if (touch.phase == TouchPhase.Began) 
+			{
+				Ray ray = Camera.main.ScreenPointToRay(touch.position);
+				RaycastHit hit;
+				if (Physics.Raycast (ray, out hit)) 
+				{
+//					Debug.Log(hit.collider.gameObject.name);
+					if (hit.collider.gameObject.name == "NhancvSite") {
+						Application.OpenURL ("https://nhancv.github.io");
+					}
+
+				}
+			}
+		}
+	}
+
+}
